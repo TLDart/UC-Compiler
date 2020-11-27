e355f6832a7f6ed9e15e481216a7da274c73a75c
Duarte Dias <duartedias@student.dei.uc.pt>
2020-11-27 19:33:44 +0000

    Added Symbol Already Defined

--- semantics.h
+++ semantics.h
@@ -7,7 +7,7 @@
 #include "print_lib.h"
 
 extern struct scope* scope_head;
-
+extern int columns, lines, yyleng;
 int check_program(struct program* head);
 void insert_default_values(struct scope* head);
 int check_f_dec(struct function_declaration* f_dec,char *name);
@@ -16,14 +16,14 @@ int check_f_def(struct function_definition* fdef);
 int check_return_type(typespec_type typ, char *name);
 int check_param_list(struct parameter_list* pl, char* name);
 int check_f_body(struct function_body* head, char *name);
-int check_statement(struct statement* head);
-int check_if(struct if_statement* head);
-int check_return(struct return_statement* rs);
-int check_while(struct while_statement* head);
-int check_statlist(struct statlist_statement* head);
-int check_expression(struct expression* exp);
-int check_op1(struct op1* op);
-int check_op2(struct op2* op);
-int check_terminal(struct terminal* t);
-int check_call(struct call* c);
+int check_statement(struct statement* head, char* name);
+int check_if(struct if_statement* head, char* name);
+int check_return(struct return_statement* rs, char* name);
+int check_while(struct while_statement* head, char* name);
+int check_statlist(struct statlist_statement* head, char* name);
+int check_expression(struct expression* exp, char* name);
+int check_op1(struct op1* op, char* name);
+int check_op2(struct op2* op, char* name);
+int check_terminal(struct terminal* t, char* name);
+int check_call(struct call* c, char* name);
 #endif
